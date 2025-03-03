1dfd64438938f097ee606a1c0d1ba638b021db5c
redhoodsu <redhoodsu@tencent.com>
2025-03-03 20:43:30 +0800

    feat: add theme and language

---
+++ main/lib/language.ts
@@ -0,0 +1,27 @@
+import { getSettingsStore } from '../../../main/lib/store'
+import { i18n, hasLocale } from '../../../common/util'
+import { app } from 'electron'
+import { handleEvent } from 'share/main/lib/util'
+import log from 'share/common/log'
+
+const logger = log('language')
+
+const store = getSettingsStore()
+
+let language = 'en-US'
+export function get() {
+  return language
+}
+
+export function init() {
+  logger.info('init')
+
+  const lang = store.get('language')
+  let systemLanguage = 'en-US'
+  if (hasLocale(app.getLocale())) {
+    systemLanguage = app.getLocale()
+  }
+  language = lang === 'system' ? systemLanguage : lang
+  i18n.locale(language)
+  handleEvent('getLanguage', get)
+}

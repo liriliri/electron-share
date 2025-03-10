571f39d862e8ab1fb203aa42003f8b213e96fc3f
redhoodsu <redhoodsu@tencent.com>
2025-03-10 18:01:37 +0800

    feat: add components

--- common/types.ts
+++ common/types.ts
@@ -8,7 +8,7 @@ import {
 export type IpcGetLanguage = () => string
 export type IpcGetTheme = () => string
 export type IpcGetStore = (name: string) => any
-export type IpcSetStore = (name: string, val: any) => any
+export type IpcSetStore = (name: string, val: any) => void
 export type IpcShowContextMenu = (x: number, y: number, template: any) => void
 export type IpcShowOpenDialog = (
   options: OpenDialogOptions

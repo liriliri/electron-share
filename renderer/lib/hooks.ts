4977eb1c8fc40e7351e023ed4cc0a5e3a5147ad0
redhoodsu <redhoodsu@tencent.com>
2025-06-12 13:34:36 +0800

    chore: small changes

--- renderer/lib/hooks.ts
+++ renderer/lib/hooks.ts
@@ -2,7 +2,6 @@ import { useEffect } from 'react'
 
 export function useWindowResize(resizeCallback: () => void) {
   useEffect(() => {
-    resizeCallback()
     window.addEventListener('resize', resizeCallback)
 
     return () => {

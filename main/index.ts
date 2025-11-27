656dbbe8840542d13d2c2765ce553de1a3bf66d9
surunzi <surunzi@foxmail.com>
2025-11-27 23:51:17 +0800

    fix: disable spellchecker for linux

--- main/index.ts
+++ main/index.ts
@@ -1,4 +1,4 @@
-import { app } from 'electron'
+import { app, session } from 'electron'
 import isMac from 'licia/isMac'
 import * as theme from './lib/theme'
 import * as language from './lib/language'
@@ -6,6 +6,7 @@ import * as ipc from './lib/ipc'
 import * as updater from './lib/updater'
 import pkg from '../../../package.json'
 import { setupTitlebar } from 'custom-electron-titlebar/main'
+import isWindows from 'licia/isWindows'
 
 if (!app.requestSingleInstanceLock()) {
   app.quit()
@@ -19,6 +20,9 @@ if (!isMac) {
 app.setName(pkg.productName)
 
 app.on('ready', () => {
+  if (!isMac && !isWindows) {
+    session.defaultSession.setSpellCheckerLanguages([])
+  }
   setupTitlebar()
   language.init()
   theme.init()
